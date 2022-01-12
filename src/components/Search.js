131b137476498e3f7b9ad3d3fb5160a2f776e615
ammar <ammarraneez@gmail.com>
2022-01-12 21:51:03 +0530

    feat: initial axios w wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Search() {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      search
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
+      </div>
     </div>
   );
 }
